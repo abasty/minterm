678be781920085f8608505fed0978580d134b86d
Alain BASTY <alain.basty@free.fr>
2024-08-30 23:52:54 +0200

    Added Minitel bitmap fonts tools and definition

---
+++ tools/xtel2minterm.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bdfparser import Font, Bitmap, Glyph
+from PIL import Image
+
+# Do not consider this file: 'g28x10.bdf'
+
+for file in ['g08x10.bdf', 'g18x10.bdf']:
+    # Load the font
+    font = Font(file)
+    width = font.headers['fbbx']
+    height = font.headers['fbby']
+
+    # Print some information
+    print(f"The '{file}' font's global size is "
+        f"{font.headers['fbbx']} x {font.headers['fbby']} pixels, "
+        f"it contains {len(font)} glyphs.")
+
+    columns = []
+    column = []
+    for cp in range(0, 128):
+        if (cp not in font.glyphs):
+            glyph: Glyph = font.glyphbycp(0x20)
+            print(f"Glyph for codepoint {cp} is not available, fallback to space (0x20).")
+        else:
+            glyph: Glyph = font.glyphbycp(cp)
+            print(f'Glyph for codepoint {cp} exists.')
+        # end if
+
+        # Draw the glyph
+        bitmap: Bitmap = glyph.draw()
+
+        # Add bitmap in the current column
+        column.append(bitmap)
+
+        if (len(column) == 16):
+            # Add column to the list of columns
+            columns.append(Bitmap.concatall(column, direction=0))
+            column = []
+        # end if
+    # end for
+
+    alls: Bitmap = Bitmap.concatall(columns, direction=1)
+    print('Bitmap size is', alls.width(), 'x', alls.height(), 'pixels.')
+
+    # Convert the bitmap to an image
+    image: Image = Image.frombytes('1', (alls.width(), alls.height()), alls.tobytes('1'))
+
+    # Remove font extension from the filename and add save as .png
+    image.save(file.replace('.bdf', '.png'))
+# end for
