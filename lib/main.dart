8fb94e9b85ad928d360f962313d84eae6bd066f8
Alain BASTY <alain.basty@free.fr>
2024-09-17 00:18:59 +0200

    Added "Send file"

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:file_picker/file_picker.dart';
 
 import 'min_model.dart';
 import 'min_widget.dart';
@@ -44,44 +45,96 @@ class MainApp extends StatelessWidget {
       ),
       home: ChangeNotifierProvider<MinModel>(
         create: (context) => MinModel(),
-        child: Scaffold(
+        child: const Scaffold(
           body: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      var scale = MinSettings().scale + 0.5;
-                      if (scale > 4) scale = 0.5;
-                      MinSettings.setScale(scale);
-                    },
-                    child: const Text('Scale'),
-                  ),
-                  Consumer<MinModel>(
-                    builder: (context, minmodel, child) => ElevatedButton(
-                      onPressed: () {
-                        minmodel.emulate([0x41, 0x42, 0x43]);
-                      },
-                      child: const Text('Send ABC'),
-                    ),
-                  ),
-                  Consumer<MinModel>(
-                    builder: (context, minmodel, child) => ElevatedButton(
-                      onPressed: () {
-                        minmodel.emulate([0x0C]);
-                      },
-                      child: const Text('Clear'),
-                    ),
-                  ),
-                ],
-              ),
-              // Add text widget
-              const MinWidget(),
+              Row(children: [Scale(), Clear(), SendABC(), SendFile()]),
+              MinWidget(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SendABC extends StatelessWidget {
+  const SendABC({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MinModel>(
+      builder: (context, minmodel, child) => ElevatedButton(
+        onPressed: () {
+          minmodel.emulate([0x41, 0x42, 0x43]);
+        },
+        child: const Text('Send ABC'),
+      ),
+    );
+  }
+}
+
+class Clear extends StatelessWidget {
+  const Clear({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MinModel>(
+      builder: (context, minmodel, child) => ElevatedButton(
+        onPressed: () {
+          minmodel.emulate([0x0C]);
+        },
+        child: const Text('Clear'),
+      ),
+    );
+  }
+}
+
+class Scale extends StatelessWidget {
+  const Scale({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        var scale = MinSettings().scale + 0.5;
+        if (scale > 4) scale = 0.5;
+        MinSettings.setScale(scale);
+      },
+      child: const Text('Scale'),
+    );
+  }
+}
+
+class SendFile extends StatelessWidget {
+  const SendFile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MinModel>(
+      builder: (context, minmodel, child) => ElevatedButton(
+        onPressed: () async {
+          FilePickerResult? result = await FilePicker.platform.pickFiles(
+            initialDirectory: '/home/alain/Projects/minitel/new-zboub/pages/',
+            allowedExtensions: ['tel'],
+          );
+          if (result != null) {
+            final file = File(result.files.single.path!);
+            final codes = file.readAsBytesSync();
+            minmodel.emulate(codes);
+          }
+        },
+        child: const Text('Send file...'),
       ),
     );
   }
