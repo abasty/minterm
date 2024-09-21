9d8b6d2a2c4fbb26b9d71ebf0599891d3f9ffe08
Alain BASTY <alain.basty@free.fr>
2024-09-21 23:08:45 +0200

    Removed unneeded background drawing

--- lib/min_widget.dart
+++ lib/min_widget.dart
@@ -260,12 +260,6 @@ class _MinPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    // Clear the canvas with a black color
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-      Paint()..color = Colors.black,
-    );
-
     if (!MinSettings().isLoaded) return;
 
     // Draw the screen
