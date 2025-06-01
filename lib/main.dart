35fa05e9c56d85f55c22f4beabdb9e229a96b55d
Alain BASTY <alain.basty@free.fr>
2025-06-02 00:49:04 +0200

    Fixed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:path/path.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 import 'min_emulator.dart';
