bd1c323d54fc408b4a4370aec57cfb69ebb4cba6
Alain BASTY <alain.basty@free.fr>
2026-08-30 19:32:15 +0200

    Refactor menu titles for consistency: update 'Ports série' to 'Ports série...' and 'Connexions' to 'Services'

--- lib/min_serial.dart
+++ lib/min_serial.dart
@@ -10,7 +10,7 @@ class ConnectionSerial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('Ports série'),
+      title: const Text('Ports série...'),
       onTap: () {
         Navigator.push(
           context,
