59925fa4fb242b2bdbc8404d9bc639917a87e4bc
Alain BASTY <alain.basty@free.fr>
2026-09-15 21:48:27 +0200

    Deploy e7f31a5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "216345529"
+    serviceWorkerVersion: "3514780907"
   }
 });
