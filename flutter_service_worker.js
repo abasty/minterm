97bda3d62e283f3c05c5190ef6f7e82cba0d0ff2
Alain BASTY <alain.basty@free.fr>
2026-09-18 22:55:13 +0200

    Deploy 91947eb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "e9dec93214fd9f7f96d4824d0d995be4",
-"flutter_bootstrap.js": "47c3b8aee58b126226d301021c9f8839",
-"main.dart.js": "6d9dfefd67b2427c8c8bf9f58c21aa28",
+"flutter_bootstrap.js": "8258871f82f489634bd1fb6d5242246d",
+"main.dart.js": "9f5188b5b389254c9e3d41eadf32a7a5",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "version.json": "8f6874e22affbc7860a9cbe77afb3bed",
 "icons/Icon-512.png": "4e6b1a6ce79a0c39656d379392146c73",
